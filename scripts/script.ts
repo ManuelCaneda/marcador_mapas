f21a28ce02c7786caca74b75bcbca04085551173
CBManuelCSP <manuelcanedab@gmail.com>
2026-04-13 02:28:56 +0200

    Comienzo del desarrollo
    TypeScript, primeras funciones, menú hamburguesa casi terminado.

--- scripts/script.ts
+++ scripts/script.ts
@@ -1,21 +1,40 @@
+declare const L: typeof import('leaflet');
+
+export { };
+
 // ! ------ Variables del DOM ------
-const $d = document;
-const $listaMarcas = $d.querySelector(".lista-marcas");
-let listaCategorias = [
-    { nombre: "Categoría 1", color: "#aa0000" },
-    { nombre: "Categoría 2", color: "#00aa00" }
+const $d: Document = document;
+const $listaMarcas: HTMLElement | null = $d.querySelector(".lista-marcas");
+
+type Categoria = {
+    id: string;
+    nombre: string;
+    color: string;
+};
+
+let listaCategorias: Categoria[] = [
+    { id: "", nombre: "Categoría 1", color: "#aa0000" },
+    { id: "", nombre: "Categoría 2", color: "#00aa00" }
 ];
+
 // Crear el mapa centrado en una coordenada [lat, lng] con zoom inicial
-const map = L.map('map').setView([40.4166327886885, -3.698178732449527], 10);
+const map: L.Map = L.map('map').setView(
+    [40.4166327886885, -3.698178732449527],
+    10
+);
+
 // Agregar capa de tiles (fondo del mapa)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© manuel_zhurong'
 }).addTo(map);
+
 // ! ------ FUNCIONES ------
+
 // ! ------ CATEGORÍAS ------
-function pintarListaCategorias(listaCategorias) {
-    if (!$listaMarcas)
-        return;
+function pintarListaCategorias(listaCategorias: Categoria[]): void {
+
+    if (!$listaMarcas) return;
+
     $listaMarcas.innerHTML = listaCategorias.reduce((anterior, actual) => {
         return anterior + `<div class="elemento-lista-categorias p-2 d-flex justify-content-between align-items-center"
     style="border: 1px solid #eee; border-left: 3.5px solid ${actual.color}; border-radius: 5px;">
@@ -27,12 +46,15 @@ function pintarListaCategorias(listaCategorias) {
     </div>`;
     }, "");
 }
+
 // ! ------ EVENTOS ------
 $d.addEventListener("DOMContentLoaded", () => {
+
     pintarListaCategorias(listaCategorias);
-    map.on('click', (e) => {
+
+    map.on('click', (e: L.LeafletMouseEvent) => {
         const { lat, lng } = e.latlng;
         console.log('Coordenadas:', [lat, lng]);
     });
+
 });
-export {};
